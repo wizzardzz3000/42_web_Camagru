06073dfcf4b41cddcbba9f3638fb9a9d4553f841
Mscaglia <31767776+Scaglia3000@users.noreply.github.com>
2019-04-28 12:10:54 +0200

    push 28 avril

--- model/UserManager.php
+++ model/UserManager.php
@@ -13,69 +13,30 @@ class UserManager extends Manager
 
     public function saveUser()
     {
-        // to db
         session_start();
-        $con = mysqli_connect("127.0.0.1", "root", "123456", "rush00");
+        $db = $this->dbConnect($user_name, $user_email, $user_password);
 
-        // to database
-
-        if ($_POST['login'] !== '' && $_POST['passwd'] !== '' && $_POST['submit'] === 'OK') {
-            $user_login = $_POST['login'];
-            $user_pwd = hash('whirlpool', $_POST["passwd"]);
+        if ($user_name !== '' && $user_email !== '' && $user_password !== '') {
+            $user_pwd = hash('whirlpool', $user_password);
          
-            $query_check = "SELECT customer_login FROM customers WHERE customer_login='$user_login'";
-            $run = mysqli_query($con, $query_check);
+            $query_check = "SELECT user_name FROM users WHERE user_name='$user_name'";
+            $run = mysqli_query($db, $query_check);
             $check = mysqli_num_rows($run);
          
             if ($check) {
-              $_SESSION["user"] = $user_login;
+              //$_SESSION["user"] = $user_login;
+              alert('This user already exists, please login or create an account with a different name.');
             } else 
             {
-                $query = "INSERT INTO customers
-                        SET customer_login = '$user_login', 
-                        customer_passwd = '$user_pwd'";
-                 $run_pro = mysqli_query($con, $query);
+                $query = "INSERT INTO users
+                        SET user_name = '$user_name', 
+                        user_email = '$user_email',
+                        user_password = '$user_pwd'";
+                 $run_pro = mysqli_query($db, $query);
                 if (!$run_pro) {
-                    die("ERROR: ". mysqli_error($con));
+                    die("ERROR: ". mysqli_error($db));
                 }
             }
-        }
-        // to cookie
-        $account = "private/passwd";
-        if ($_POST['login'] !== '' && $_POST['passwd'] !== '' && $_POST['submit'] === 'OK')
-        {
-            if (strlen($_POST['passwd']) < 5) {
-                // header("Location: create.html");
-                exit;
-            } 
-            $user = [
-                "login" => $_POST['login'],
-                "passwd" => hash("whirlpool", $_POST['passwd'])
-            ];
-            if (file_exists($account))
-            {
-                $data = unserialize(file_get_contents($account));
-                foreach($data as $instance)
-                {
-                    if($user['login'] === $instance['login'])
-                    {
-                        // header("Location: create.html");
-                        exit; 
-                    }
-                }
-                $data[] = $user;
-            }  else {
-                if (!file_exists('private'))
-                    mkdir('private', 0755);
-                $data[] = $user;
-            }
-            file_put_contents($account, serialize($data));
-            // header("Location: login.html");
-        }
-        else
-        {   
-            // header("Location: create.html");
-            return; 
         }
     }
 
