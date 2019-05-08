c949bcfe4e2df69efcad704e605e3ccb6e9a75e0
Mscaglia <31767776+Scaglia3000@users.noreply.github.com>
2019-05-08 11:52:11 +0200

    push 8 mai 2

--- controller/pictureController.php
+++ controller/pictureController.php
@@ -3,6 +3,7 @@ session_start();
 require_once $_SERVER['DOCUMENT_ROOT'].'/model/PictureManager.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/model/UserManager.php';
 
+// LOAD THE CAMERA VIEW
 function showMainView()
 {
     $galleryManager = new PictureManager();
@@ -14,10 +15,11 @@ function showMainView()
     require('view/mainView.php');
 }
 
+// SAVE THE PICTURE (CALLED FROM AJAX)
 if(isset($_POST['img']))
 {
-    $data = $_POST['img'];
-    saveData($data);
+    $image = $_POST['img'];
+    saveData($image);
 }
 
 function saveData($image)
@@ -31,18 +33,18 @@ function saveData($image)
         $user_id = $user['user_id'];
     }
 
-    $img = $image;
-    $img = str_replace('data:image/png;base64,', '', $img);
-    $img = str_replace(' ', '+', $img);
-    $data = base64_decode($img);
+    $image = str_replace('data:image/png;base64,', '', $image);
+    $image = str_replace(' ', '+', $image);
+    $data = base64_decode($image);
     $time = time();
-    $name = $time . '.png';
-    $output = '../pictures/' . $name;
+    $file_name = $time . '.png';
+    $output = '../pictures/' . $file_name;
     file_put_contents($output, $data);
 
-    if ($pic = $galleryManager->savePictures($user_id, $name))
+    if ($pic = $galleryManager->savePictures($user_id, $file_name))
     {
         header('Location: index.php?view=camera');
+        //ajaxify !
     }
     // error catch ?
 }
--- view/mainView.php
+++ view/mainView.php
@@ -2,51 +2,57 @@
     ob_start();
 ?>
 
-<div class="main">
-    <div class="filters_list">
-        <p>filter 1</p>
-        <p>filter 2</p>
-        <p>filter 3</p>
+<div class="filters_list">
+    <p class="filter">filter 1</p>
+    <p class="filter">filter 2</p>
+    <p class="filter">filter 3</p>
+</div>
+
+<div class="mainView">
+
+    <div class="main1">
+        <video id="video" autoplay></video>
+        <br>
+        <button id="startbutton">Prendre une photo</button>
+        <br>
+        <canvas id="canvas"></canvas>
     </div>
-    <video id="video" autoplay></video>
-    <br>
-    <button id="startbutton">Prendre une photo</button>
-    <br>
-    <canvas id="canvas"></canvas>
-</div>
 
-<div class="main">
-    <p>Your pictures</p>
-    <?php
-        $picture = $gallery->fetchAll();
-        $user = $users->fetchAll();
+    <div class="main2">
+        <p>Your pictures</p>
+        <?php
+            $picture = $gallery->fetchAll();
+            $user = $users->fetchAll();
 
-        for ($i = 0; $user[$i]; $i++)
-        {
-            if ($user[$i]['user_name'] == $_SESSION['loggued_on_user'])
+            for ($i = 0; $user[$i]; $i++)
             {
-                $user_id = $user[$i]['user_id'];
+                if ($user[$i]['user_name'] == $_SESSION['loggued_on_user'])
+                {
+                    $user_id = $user[$i]['user_id'];
+                }
             }
-        }
-        for ($i = 0; $picture[$i]; $i++)
-        {
-            if ($picture[$i]['user_id'] == $user_id)
+            for ($i = 0; $picture[$i]; $i++)
             {
-    ?>
-            <div id='full_product'>
-                <div id='single_product'>
-                    <a href="index.php?view=picture&id=<?= $picture[$i]['picture_id'] ?>">
-                        <?php
-                            echo '<img class="gallery_picture" src="pictures/'.$picture[$i]['content'].'"/>';
-                        ?>
-                    </a>
+                if ($picture[$i]['user_id'] == $user_id)
+                {
+        ?>
+                <div id='full_product_small'>
+                    <div id='single_product_small'>
+                        <a href="index.php?view=picture&id=<?= $picture[$i]['picture_id'] ?>">
+                            <?php
+                                echo '<img class="gallery_picture" src="pictures/'.$picture[$i]['content'].'"/>';
+                            ?>
+                        </a>
+                    </div>
                 </div>
-            </div>
-    <?php
+        <?php
+                }
             }
-        }
-    ?>
+        ?>
+    </div>
+
 </div>
+
 
 <script src="javascript/take_pic.js"></script>
 
